1df667016dc6cb015eaf0e410cddffd69a67c413
haxilla <haxilla@yahoo.com>
2020-03-31 14:15:56 -0700

    datafix in progress

--- app/rets/matrix/GLVAR/datafix/GLVAR_datafix.php
+++ app/rets/matrix/GLVAR/datafix/GLVAR_datafix.php
@@ -45,33 +45,10 @@
   ALTER TABLE GLVAR_Homes
   MODIFY MatrixModifiedDT timestamp;
 ");
-/*
-//GLVAR_Homes changes
-Schema::connection('rets')
-->table('GLVAR_Homes', function (Blueprint $table) {
-    $table->timestamp('MatrixModifiedDT')->change();
-		$table->timestamp('PhotoModificationTimeStamp')->change();
-		$table->timestamp('PriceChangeTimestamp')->change();
-		$table->timestamp('PriceChgDate')->change();
-		$table->timestamp('OriginalEntryTimestamp')->change();
-		$table->timestamp('NODDate')->change();
-});
-Schema::connection('rets')
-->table('GLVAR_Homes_backup', function (Blueprint $table) {
-    $table->timestamp('MatrixModifiedDT')->change();
-		$table->timestamp('PhotoModificationTimeStamp')->change();
-		$table->timestamp('PriceChangeTimestamp')->change();
-		$table->timestamp('PriceChgDate')->change();
-		$table->timestamp('OriginalEntryTimestamp')->change();
-		$table->timestamp('NODDate')->change();
-});
-Schema::connection('rets')
-->table('GLVAR_Homes_synch', function (Blueprint $table) {
-    $table->timestamp('MatrixModifiedDT')->change();
-		$table->timestamp('PhotoModificationTimeStamp')->change();
-		$table->timestamp('PriceChangeTimestamp')->change();
-		$table->timestamp('PriceChgDate')->change();
-		$table->timestamp('OriginalEntryTimestamp')->change();
-		$table->timestamp('NODDate')->change();
-});
-*/
+
+
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes_backup
+  MODIFY PhotoModificationTimestamp datetime null;
+");
