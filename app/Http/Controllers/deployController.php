40ca1fb0fdd9fb0a2aad03f5aa4b8b608d9a7a53
haxilla <haxilla@yahoo.com>
2020-03-31 15:45:17 -0700

    deploy symfony fix

--- app/Http/Controllers/deployController.php
+++ app/Http/Controllers/deployController.php
@@ -50,7 +50,8 @@ class deployController extends Controller
         	$ref=$payload->ref;
         	if($ref=='refs/heads/master'){
         		$root_path = base_path();
-                  		$process = new Process('cd ' . $root_path . '; ./deploy.sh');
+                  		//$process = new Process('cd ' . $root_path . '; ./deploy.sh');
+                      $process = Process::fromShellCommandline('cd ' . $root_path . '; ./deploy.sh');
                   		$process->run(function ($type, $buffer) {
                      			echo $buffer;
                   		});
