bf6a61a0da92bd49a8b5701efc05897f6ffee107
haxilla <haxilla@yahoo.com>
2020-03-31 14:00:20 -0700

    datafix GLVAR_Homes section complete

--- app/rets/matrix/GLVAR/datafix/GLVAR_datafix.php
+++ app/rets/matrix/GLVAR/datafix/GLVAR_datafix.php
@@ -10,7 +10,41 @@
   ALTER TABLE GLVAR_Homes
   MODIFY TStatusDate timestamp;
 ");
-
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY StatusChangeTimestamp timestamp;
+");
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY ProviderModificationTimestamp timestamp;
+");
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY PriceChangeTimestamp timestamp;
+");
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY PriceChgDate timestamp;
+");
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY OriginalEntryTimestamp timestamp;
+");
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY NODDate timestamp;
+");
+\DB::connection('rets')
+->statement("
+  ALTER TABLE GLVAR_Homes
+  MODIFY MatrixModifiedDT timestamp;
+");
 /*
 //GLVAR_Homes changes
 Schema::connection('rets')
