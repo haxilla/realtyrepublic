b938c1a356197ec0d3b9624cac993b414288f1ae
haxilla <haxilla@yahoo.com>
2020-03-31 15:29:25 -0700

    deploy secret fix

--- config/app.php
+++ config/app.php
@@ -27,6 +27,7 @@ return [
     */
 
     'env' => env('APP_ENV', 'production'),
+    'deploy_secret' => env('APP_DEPLOY_SECRET'),
 
     /*
     |--------------------------------------------------------------------------
