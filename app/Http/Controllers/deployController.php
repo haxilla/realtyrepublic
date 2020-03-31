af4d3935b6c6976948dcaeb8af2a21fb7a9cac11
haxilla <haxilla@yahoo.com>
2020-03-31 15:58:57 -0700

    fix

--- app/Http/Controllers/deployController.php
+++ app/Http/Controllers/deployController.php
@@ -50,11 +50,11 @@ class deployController extends Controller
         	$ref=$payload->ref;
         	if($ref=='refs/heads/master'){
         		$root_path = base_path();
-                  		//$process = new Process('cd ' . $root_path . '; ./deploy.sh');
-                      $process = Process::fromShellCommandline('cd ' . $root_path . '; ./deploy.sh');
-                  		$process->run(function ($type, $buffer) {
-                     			echo $buffer;
-                  		});
+            		//$process = new Process('cd ' . $root_path . '; ./deploy.sh');
+                $process = Process::fromShellCommandline('cd ' . $root_path . '; ./deploy.sh');
+            		$process->run(function ($type, $buffer) {
+               			echo $buffer;
+            		});
          }else{
 			      echo "$ref - Nothing pulled - Not on Master Branch";
 
