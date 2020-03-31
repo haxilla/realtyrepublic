c9869fae7fc5c231ca393f1b4bea76f6784c2de4
haxilla <haxilla@yahoo.com>
2020-03-31 12:22:39 -0700

    catchup

--- app/rets/matrix/GLVAR/compare/compare_index.php
+++ app/rets/matrix/GLVAR/compare/compare_index.php
@@ -1,5 +1,10 @@
 <?php
 
+//error reporting
+error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+ini_set("display_errors", 1);
+
 //checks logs for retsLoop
 include("compare_checklog.php");
 
@@ -17,6 +22,7 @@ if($retsLoop==$startLogName){
 }else{
 
 	// table names
+
 	// ex. $nowHomes=GLVAR_Homes
 	${'now'.$retsClass} = $mlsName.'_'.$retsClass;
 	// ex. $oldHomes=GLVAR_Homes_backup
@@ -29,5 +35,62 @@ if($retsLoop==$startLogName){
 	include("compare_loop.php");
 	//Updates & sends JSON
 	include("compare_JSON.php");
-	
+
 }
+
+/* ALTER TABLES WHEN COMPLETE */
+
+/*
+/* Homes */
+/*
+MatrixModifiedDT
+PhotoModificationTimestamp
+PriceChangeTimeStamp
+PriceChgDate
+OriginalEntryTimestamp
+NODDate
+----------------------
+GLVAR_Homes,
+GLVAR_Homes_backup,
+GLVAR_Homes_synch,
+
+
+*/
+/* Agents */
+/*
+PhotoModificationTimestamp
+ProviderModificationTimestamp
+MatrixModifiedDT
+-----------------------
+GLVAR_Agents,
+GLVAR_Agents_backup,
+GLVAR_Agents_synch
+
+*/
+/* Offices */
+/*
+MembershipDate
+MatrixModifiedDT
+TerminationDate
+-------------------------
+GLVAR_Offices,
+GLVAR_Offices_backup,
+GLVAR_Offices_synch
+*/
+/*
+
+//Column Type
+//
+USE rets
+
+ALTER TABLE GLVAR_Agents
+MODIFY PhotoModificationTimestamp timestamp
+
+//Value
+//
+UPDATE GLVAR_Agents
+SET PhotoModificationTimestamp=NULL
+WHERE PhotoModificationTimestamp
+LIKE '%%0000-00%';
+
+*/
