b73012c6df4899355c8ac5c75ef4914e38e5230a
haxilla <haxilla@yahoo.com>
2020-04-01 13:03:32 -0700

    testing auto deploy

--- deploy.sh
+++ deploy.sh
@@ -12,7 +12,6 @@ composer install --no-interaction --no-dev --prefer-dist
 	# --no-interaction	Do not ask any interactive question
 	# --no-dev		Disables installation of require-dev packages.
 	# --prefer-dist		Forces installation from package dist even for dev versions.
-  # delete #
 
 # clear cache
 php artisan cache:clear
