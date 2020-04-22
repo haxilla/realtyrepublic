1969f01c87d4ab22d3640c77c61b82a527a94454
haxilla <haxilla@yahoo.com>
2020-04-22 13:59:30 -0700

    fix js passwordfix

--- public/my/js/admin/synch/passwordFix.js
+++ public/my/js/admin/synch/passwordFix.js
@@ -4,22 +4,20 @@ $(function(){
   function(e){
     //rotate icon
     $('.scriptStartButton .ticon').addClass("rotating");
-
     //remove startNow
     $('.scriptStartButton').removeClass('startNow');
-
     //icon css
     $('.scriptStartButton .ticon').css({
       "color":"#464555",
       "opacity":".5",
     });
-
     //button css
     $('.scriptStartButton').css({
       "background":"#000",
       "opacity":".5",
     });
-
+    //progressWait
+    $('.passwordFix .progressWait').show();
   });
 
 });
