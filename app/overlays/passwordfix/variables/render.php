fbb1204b1151c3619baab49319f5156985df41ed
haxilla <haxilla@yahoo.com>
2020-04-22 12:07:33 -0700

    fix

--- app/overlays/passwordfix/variables/render.php
+++ app/overlays/passwordfix/variables/render.php
@@ -4,7 +4,7 @@ Use App\models\core\propagent;
 
 $passHashCount=propagent::select('id','agtPswd')
 ->whereNull('passHash')
-->whereNotNull('agtPswd');
+->whereNotNull('agtPswd')
 ->count();
 
 $html=\View::make('admin.overlays.content.passHashCount')
