9c3f5105400e8436a5346ded32ecc6bbc6422846
haxilla <haxilla@yahoo.com>
2020-04-22 12:06:32 -0700

    passwordFix in progress

---
+++ app/overlays/passwordfix/variables/render.php
@@ -0,0 +1,13 @@
+<?php
+
+Use App\models\core\propagent;
+
+$passHashCount=propagent::select('id','agtPswd')
+->whereNull('passHash')
+->whereNotNull('agtPswd');
+->count();
+
+$html=\View::make('admin.overlays.content.passHashCount')
+->with([
+	'passHashCount'	 => $passHashCount,
+]);
---
+++ resources/views/admin/overlays/content/passHashCount.blade.php
@@ -0,0 +1,13 @@
+<div class="passwordFix">
+  <div class="headWrapper">
+    <div class="mainHead">
+      PASSWORD FIX
+    </div>
+    <div class="subHead">
+      Fixes passwords from agtPswd to bcrypted passHash
+    </div>
+  </div>
+  <div class="mainContent">
+    FIXES NEEDED <span class="theCount">{{$passHashCount}}</span>
+  </div>
+</div>
