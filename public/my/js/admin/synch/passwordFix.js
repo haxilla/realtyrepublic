a9625a093761f61d47b13f52d79408a46b3923b7
haxilla <haxilla@yahoo.com>
2020-04-25 16:44:11 -0700

    fix for agtPswd php script

--- public/my/js/admin/synch/passwordFix.js
+++ public/my/js/admin/synch/passwordFix.js
@@ -48,7 +48,7 @@ function passwordFix(fixCount){
         $('.passwordFix span.theCount').text(thisCount);
         $('.passwordFix .progressWait').hide();
         $('.passwordFix .progress').show();
-        $('.passwordFIx .progress-bar').css({"width":thisPercent,})
+        $('.passwordFix .progress-bar').css({"width":thisPercent,})
         passwordFix(fixCount);
       }else{
         alert('Fixes Complete!');}
