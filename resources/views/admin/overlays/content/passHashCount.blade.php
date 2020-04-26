b8985c60f5f0a49171410bcd3cf76f008914d491
haxilla <haxilla@yahoo.com>
2020-04-25 17:00:32 -0700

    fix for agtPswd php script

--- resources/views/admin/overlays/content/passHashCount.blade.php
+++ resources/views/admin/overlays/content/passHashCount.blade.php
@@ -17,16 +17,14 @@
           Please wait ...
         </span>
       </div>
-      <div class="d-block w-100">
-        <div class="progress position-relative">
-          <div class="progress-bar progress-bar-striped
-          progress-bar-animated bg-success" role="progressbar"
-          style="width: 50%;">
-          </div>
-          <small class="progressText justify-content-center
-          d-flex position-absolute w-100">
-          </small>
+      <div class="col progress position-relative">
+        <div class="progress-bar progress-bar-striped
+        progress-bar-animated bg-success" role="progressbar"
+        style="width: 50%;">
         </div>
+        <small class="progressText justify-content-center
+        d-flex position-absolute w-100">
+        </small>
       </div>
       <div class="startNow scriptStartButton"
       data-fixcount="{{$passHashCount}}">
