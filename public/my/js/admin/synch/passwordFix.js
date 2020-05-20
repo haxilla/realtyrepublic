9e7195784575b9b242c9273ea6ca1d7e4270e328
haxilla <haxilla@yahoo.com>
2020-05-20 16:35:57 -0700

    restoring passwordFix.js

---
+++ public/my/js/admin/synch/passwordFix.js
@@ -0,0 +1,67 @@
+$(function(){
+
+  $('body').on('click','.passwordFix .startNow',
+  function(e){
+    //rotate icon
+    $('.scriptStartButton .ticon').addClass("rotating");
+    //remove startNow
+    $('.scriptStartButton').removeClass('startNow');
+    //icon css
+    $('.scriptStartButton .ticon').css({
+      "color":"#464555",
+      "opacity":".5",
+    });
+    //button css
+    $('.scriptStartButton').css({
+      "background":"#000",
+      "opacity":".5",
+    });
+
+    //progressWait show
+    $('.passwordFix .progressWait').show();
+    $('.passwordFix .progress').hide();
+
+    var fixCount=$(this).data("fixcount");
+    //run passwordFix script
+    passwordFix(fixCount);
+
+  });
+
+});
+
+function passwordFix(fixCount){
+
+  // process the form
+  $.ajax({
+     type        : 'GET', 						  // type of HTTP request
+     url         : '/synch/agtPswdFix?fixCount='+fixCount, // the url to navigate to
+     dataType    : 'json', 						  // data to expect from server
+     encode      : true
+  })
+
+  // using the done promise callback
+  .done(function(data){
+    if(data.status=='success'){
+      fixCount=data.fixCount;
+      thisCount=data.thisCount;
+      thisPercent=data.thisPercent+'%';
+
+      if(thisCount>0){
+        $('.passwordFix span.theCount').text(thisCount);
+        $('.passwordFix .progressWait').hide();
+        $('.passwordFix .progress').show();
+        $('.passwordFix .progress-bar').css({"width":thisPercent,})
+        passwordFix(fixCount);
+      }else{
+        alert('Fixes Complete!');}
+
+    }else{
+      alert('something fishy');
+    }
+  })
+
+  // using the fail promise callback
+  .fail(function(data){
+    alert('error');
+  });
+}
