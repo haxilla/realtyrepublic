efb1edd39881f8533201e394a3248df064cd65c6
haxilla <haxilla@yahoo.com>
2020-05-20 19:12:26 -0700

    added new index for mdbxPublic

---
+++ resources/views/mdbxPublic/render/unimportableSuccess.blade.php
@@ -0,0 +1,29 @@
+<div class="default overlayContent">
+
+  <div class="overlayBorder">
+
+    <div class="contentWrapper">
+
+      <div class="row noMargin headWrapper">
+        <div class="col-12 noPad
+        mainHead">
+           TRIAL REQUEST SUCCESS!
+        </div>
+        <div class="col-12 noPad
+        subHead">
+        </div>
+      </div>
+
+      <div>
+        You should receive an email shortly with
+        your log-in credentials!
+      </div>
+
+      <div class="primaryButton">
+        OK
+      </div>
+
+    </div>
+  </div>
+
+</div>
