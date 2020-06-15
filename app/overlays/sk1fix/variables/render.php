cfc16ffce2bc37df91a3e0760dfe98ac630648a1
haxilla <haxilla@yahoo.com>
2020-06-15 13:29:14 -0700

    sk1 fix in progress

--- app/overlays/sk1fix/variables/render.php
+++ app/overlays/sk1fix/variables/render.php
@@ -1,1 +1,31 @@
 <?php
+//get models
+use App\models\core\propmeta;
+use App\models\oldsite\oldFlyer;
+
+//remote server with bad sk1
+/*
+$remoteSK1=oldFlyer::select('ufid')
+->where('sk1','like','%'.'='.'%')
+->get();
+*/
+
+$localSK1=propmeta::select('propflyer_id')
+->whereNull('sk1')
+->orWhere('sk1','like','%'.'='.'%')
+->get();
+
+//gen password function (sets sk1 value)
+require_once(app_path().'/members/keygens/mdbxGenPswd.php');
+
+//local propmetas table
+foreach($localSK1 as $the){
+   //update
+   propmeta::where('propflyer_id','=',"$the->propflyer_id")
+   ->update([
+      'sk1'=>$sk1,
+   ]);
+   oldFlyer::where('ufid','=',"$the->propflyer_id")
+   ->update([
+      'sk1'=>$sk1,
+   ]);}
