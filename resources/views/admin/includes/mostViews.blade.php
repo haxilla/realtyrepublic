7f2f0632cc44f6dc92b70c76fbcd3e05412dbff4
haxilla <haxilla@yahoo.com>
2020-07-13 15:01:54 -0700

    fixes for no photos available

--- resources/views/admin/includes/mostViews.blade.php
+++ resources/views/admin/includes/mostViews.blade.php
@@ -3,15 +3,17 @@
 	<!-- surround with code below to remove flyers with broken image -->
 		<div class="indexListing">
 			<div class="defaultPhotoDiv">
-				<img src="{{env('APP_IMGURL')}}/hqphotos/{{$the->theMeta->zipDir}}/{{$the->theMeta
-				->mlsDir}}/{{$the->thePhotos->where('def','=','1')->first()->photoName}}" 
-				alt="{{$the->xFullStreet}} Main">
+				@if($the->thePhotos->where('def','=','1')->first())
+					<img src="{{env('APP_IMGURL')}}/hqphotos/{{$the->theMeta->zipDir}}/{{$the->theMeta
+					->mlsDir}}/{{$the->thePhotos->where('def','=','1')->first()->photoName}}"
+					alt="{{$the->xFullStreet}} Main">
+				@endif
 			</div>
 		</div>
 	<!-- end of surround -->
 @endforeach
 
-<!-- to remove entirely add this 
+<!-- to remove entirely add this
 @ if($the->thePhotos->first()->notFound != 1)
 @ endif
 -->
