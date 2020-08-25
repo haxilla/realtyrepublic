600793f687c85d989710cff81f7384383a25e28f
haxilla <haxilla@yahoo.com>
2020-08-24 19:05:11 -0700

    testing atomOpen

--- app/devJournal/functions/taskAtomOpen.php
+++ app/devJournal/functions/taskAtomOpen.php
@@ -1,3 +1,8 @@
 <?php
 
-echo exec('whoami');
+$old_path = getcwd();
+chdir('/var/www/larasites/realtyemails/app/devJournal/scripts');
+$output = shell_exec('atomOpen .env-dev');
+chdir($old_path);
+
+$output = shell_exec('./script.sh var1 var2');
