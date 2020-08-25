e250c91bf53833fa70ef40807a990e396050feb5
haxilla <haxilla@yahoo.com>
2020-08-24 19:17:28 -0700

    testing atomOpen

--- app/devJournal/functions/taskAtomOpen.php
+++ app/devJournal/functions/taskAtomOpen.php
@@ -1,7 +1,12 @@
 <?php
-
+/*
 $old_path = getcwd();
 //chdir('/more');
 $output = shell_exec('atomOpen.bin .env-dev');
 //chdir($old_path);
+dd($output);
+*/
+
+$output = array();
+exec('/path/to/calibrate', $output);
 dd($output);
