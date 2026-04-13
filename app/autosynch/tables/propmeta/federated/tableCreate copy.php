8270119b1de39e9b8a7a4cc63fa2418070af2f31
Your Name <you@example.com>
2026-04-13 11:00:33 -0700

    manual is reserved keyword?

--- app/autosynch/tables/propmeta/federated/tableCreate copy.php
+++ app/autosynch/tables/propmeta/federated/tableCreate copy.php
@@ -4,25 +4,6 @@ include(app_path().'/autosynch/tables/mysqlconn.php');
 
 //create federated table
 //first need to get by original field names
-
-$results = DB::statement("
-    CREATE TABLE remailsynch.$tableFed (
-        e_proptype varchar(255),
-        ufid int,
-        umid int,
-        sk1 varchar(255),
-        sysID varchar(255),
-        zipDir varchar(255),
-        mlsDir varchar(255),
-        `manual` tinyint(1),
-        PRIMARY KEY (ufid)
-    )
-    ENGINE=FEDERATED
-    DEFAULT CHARSET=latin1
-    CONNECTION='$connectString'
-");
-
-/*
 $results=DB::select( DB::raw("
   CREATE TABLE remailsynch.$tableFed (
     e_proptype  varchar(255),
@@ -39,7 +20,5 @@ $results=DB::select( DB::raw("
   DEFAULT CHARSET=latin1
   CONNECTION='$connectString';
 "));
-*/
-
 // **  connection string reference
 // **  scheme://user_name[:password]@host_name[:port_num]/db_name/tbl_name
